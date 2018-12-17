85fc8e4bd168129161d93b1de40b598bebacec14
ASC\eduardo.santos <eduardosantos.ist@gmail.com>
2018-12-17 14:56:20 -0200

    Correção de bug que permitia selecionar a mesma carta duas vezes e Adição de alguns comentários para facilitar a leitura

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let deck;
 let openCards;
 let moves;
@@ -5,12 +7,15 @@ let matches;
 let minutes, seconds;
 let timer;
 
+// Exibe todas as cartas na tela
 function displayCards() {
   shuffle(deck).forEach(function(card) {
     $('.deck').append(`<li class="card"><i class="fas ${card}"></i></li>`);
   });
 }
 
+
+// Embaralha um array de Cartas
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -25,6 +30,7 @@ function shuffle(array) {
   return array;
 }
 
+// Vira uma Carta quando clicada
 function openCard(card) {
 
   card.classList.add('open');
@@ -35,8 +41,9 @@ function openCard(card) {
 
 }
 
+// Responsável pela parte de Match das cartas e controle de alguns Scores
 function checkOpenCards(){
-  if (openCards.length === 2){
+  if (openCards.length === 2) {
     if (openCards[0].firstChild.className === openCards[1].firstChild.className) {
 
       openCards.forEach(function(openCard){
@@ -66,6 +73,7 @@ function checkOpenCards(){
   showMovesCount();
 }
 
+// Exibe o contador de Estrelas no Score Principal
 function showStars() {
   let maxStarScore = 5;
 
@@ -74,14 +82,17 @@ function showStars() {
   }
 }
 
+// Exibe o contador de Movimentos no Score Principal
 function showMovesCount() {
   $('.moves').text(moves);
 }
 
+// Incrementa o contador de movimentos
 function increaseMovesCount(){
   moves ++;
 }
 
+// Responsável por atualizar visualmente o Score e chamar função que verifica o fim do jogo
 function updateStarScore() {
   let starsArray = $('.stars').children().toArray();
 
@@ -96,6 +107,8 @@ function updateStarScore() {
   checkGameOver();
 }
 
+// Verifica o fim do jogo
+
 function checkGameOver() {
   if (moves >= 21) {
     gameOver(false);
@@ -104,13 +117,13 @@ function checkGameOver() {
   }
 }
 
+// Exibe modal de Vitória/Derrota com as informações da pontuação do jogador
 function gameOver(win){
   let score, starsList, star;
 
   $('.score-time').html($('.timer').html());
 
   score = $('#main-score-stars').children().not('.disabled').toArray();
-  console.log(score.length);
   star = "<li><i class='fas fa-star'></i></li>";
   starsList = $('#modal-score').empty();
   score.forEach(function(){
@@ -130,6 +143,7 @@ function gameOver(win){
   showModal();
 }
 
+// Inicia o Timer
 function startTimer() {
   timer.start();
   timer.addEventListener('secondsUpdated', function (e) {
@@ -137,21 +151,24 @@ function startTimer() {
   });
 }
 
+// Exibe Modal
 function showModal() {
   $('.modal').css('display', 'flex');
 }
 
+// Esconde Modal
 function hideModal() {
   $('.modal').css('display', 'none');
 }
 
+// Inicia o Jogo
 $(function startGame(){
   $('.restart').click(function (){
     hideModal();
     startGame(location.reload());
   });
 
-  $('.deck').click('.card', function(event) {
+  $('.deck').on('click', 'li' , function(event) {
     openCard(event.target, openCards);
   });
 
